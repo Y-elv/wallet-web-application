b3f185fc28b78ff26d768ad404c3ba7d48c22134
mugishaelvis456@gmail.com <mugishaelvis456@gmail.com>
2025-01-11 19:27:48 +0200

    set up folder structure

---
+++ server/src/main/java/com/Wallet_web_application/Wallet_Web_Application/WalletWebApplication.java
@@ -0,0 +1,13 @@
+package com.Wallet_web_application.Wallet_Web_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletWebApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/Wallet_web_application/Wallet_Web_Application/WalletWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wallet_web_application.Wallet_Web_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
